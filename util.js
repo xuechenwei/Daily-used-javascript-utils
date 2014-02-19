f12da65abbe0ff528b80331cb7ac7c90eb184252
xuechengwei <xuechengweiyx@163.com>
2014-02-19 09:34:18 +0800

    add main script file

---
+++ util.js
@@ -0,0 +1,348 @@
+/*
+ * @description: 前端常用方法 --from professional javascript for web developers -by Nicholas C. Zakas
+ */
+
+
+
+function isUndefined(variable) {
+    return typeof variable == "undefined";
+}
+
+/**
+ * 断言，如果条件不符合，则抛出
+ * @param  {Expression} condition
+ * @param  {String} message   不符合条件时抛出的错误信息
+ * @return {undefined}
+ */
+function assert(condition, message) {
+    if (!condition) {
+        throw new Error(message);
+    };
+}
+
+/**
+ * 给url末尾添加get参数
+ * @param {String} url   被添加的url
+ * @param {String} key
+ * @param {String} value
+ */
+function addURLParam(url, key, value) {
+    url += (url.indexOf("?") == -1 ? "?" : "&");
+    url += encodeURIComponent(key) + "=" + encodeURIComponent(value);
+    return url;
+}
+
+/**
+ * 表单序列化
+ * @param  {String} form 被序列化的表单的id
+ * @return {[String]}      序列化后的字符串
+ */
+function serialize(formId) {
+    var form = document.getElementById(formId)
+    parts = [],
+        field = null,
+        i,
+        len,
+        j,
+        optLen,
+        option,
+        optValue;
+    for (i = 0, len = form.elements.length; i < len; i++) {
+        field = form.elements[i];
+        switch (field.type) {
+            case "select-one":
+            case "select-multiple":
+                if (field.name.length) {
+                    for (j = 0, optLen = field.options.length; j < optLen; j++) {
+                        option = field.options[j];
+                        if (option.selected) {
+                            optValue = "";
+                            if (option.hasAttribute) {
+                                optValue = (option.hasAttribute("value") ?
+                                    option.value : option.text);
+                            } else {
+                                optValue = (option.attributes["value"].specified ?
+                                    option.value : option.text);
+                            }
+                            parts.push(encodeURIComponent(field.name) + "=" +
+                                encodeURIComponent(optValue));
+                        }
+                    }
+                }
+                break;
+            case undefined: //字段集
+            case "file": //文件输入
+            case "submit": //提交按钮
+            case "reset": //重置按钮
+            case "button": //自定义按钮
+                break;
+            case "radio": //单选按钮
+            case "checkbox": //复选框
+                if (!field.checked) {
+                    break;
+                }
+                /* 执行默认操作 */
+            default:
+                //不包含没有名字的表单字段
+                if (field.name.length) {
+                    parts.push(encodeURIComponent(field.name) + "=" +
+                        encodeURIComponent(field.value));
+                }
+        }
+    }
+    return parts.join("&");
+}
+
+/**
+ * 跨浏览器创建xhr对象
+ * @return {XHR}
+ */
+function createXHR() {
+    if (typeof XMLHttpRequest != "undefined") {
+        return new XMLHttpRequest();
+    } else if (typeof ActiveXObject != "undefined") {
+        if (typeof arguments.callee.activeXString != "string") {
+            var versions = ["MSXML2.XMLHttp.6.0", "MSXML2.XMLHttp.3.0",
+                "MSXML2.XMLHttp"
+            ],
+                i, len;
+            for (i = 0, len = versions.length; i < len; i++) {
+                try {
+                    new ActiveXObject(versions[i]);
+                    arguments.callee.activeXString = versions[i];
+                    break;
+                } catch (ex) {
+                    //跳过
+                }
+            }
+        }
+        return new ActiveXObject(arguments.callee.activeXString);
+    } else {
+        throw new Error("No XHR object available.");
+    }
+}
+
+/**
+ * 跨浏览器的eventUtil
+ * @type {Object}
+ */
+var EventUtil = {
+    addHandler: function(element, type, handler) {
+        if (element.addEventListener) {
+            element.addEventListener(type, handler, false);
+        } else if (element.attachEvent) {
+            element.attachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = handler;
+        }
+    },
+    removeHandler: function(element, type, handler) {
+        if (element.removeEventListener) {
+            element.removeEventListener(type, handler, false);
+        } else if (element.detachEvent) {
+            element.detachEvent("on" + type, handler);
+        } else {
+            element["on" + type] = null;
+        }
+    },
+    getEvent: function(event) {
+        return event ? event : window.event;
+    },
+    getTarget: function(event) {
+        return event.target || event.srcElement;
+    },
+    preventDefault: function(event) {
+        if (event.preventDefault) {
+            event.preventDefault();
+        } else {
+            event.returnValue = false;
+        }
+    },
+    stopPropagation: function(event) {
+        if (event.stopPropagation) {
+            event.stopPropagation();
+        } else {
+            event.cancelBubble = true;
+        }
+    },
+    getRelatedTarget: function(event) {
+        if (event.relatedTarget) {
+            return event.relatedTarget;
+        } else if (event.toElement) {
+            return event.toElement;
+        } else if (event.fromElement) {
+            return event.fromElement;
+        } else {
+            return null;
+        }
+    },
+    getButton: function(event) {
+        if (document.implementation.hasFeature("MouseEvents", "2.0")) {
+            return event.button;
+        } else {
+            switch (event.button) {
+                case 0:
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                    return 0;
+                case 2:
+                case 6:
+                    return 2;
+                case 4:
+                    return 1;
+            }
+        }
+    },
+    getWheelDelta: function(event) {
+        if (event.wheelDelta) {
+            return (client.engine.opera && client.engine.opera < 9.5 ? -event.wheelDelta : event.wheelDelta);
+        } else {
+            return -event.detail * 40;
+        }
+    },
+    getCharCode: function(event) {
+        if (typeof event.charCode == "number") {
+            return event.charCode;
+        } else {
+            return event.keyCode;
+        }
+    },
+    getClipboardText: function(event) {
+        var clipboardData = (event.clipboardData || window.clipboardData);
+        return clipboardData.getData("text");
+    },
+    setClipboardText: function(event, value) {
+        if (event.clipboardData) {
+            return event.clipboardData.setData("text/plain", value);
+        } else if (window.clipboardData) {
+            return window.clipboardData.setData("text", value);
+        }
+    }
+};
+
+var CookieUtil = {
+
+    get: function(name) {
+        var cookieName = encodeURIComponent(name) + "=",
+            cookieStart = document.cookie.indexOf(cookieName),
+            cookieValue = null;
+        if (cookieStart > -1) {
+            var cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
+        }
+        return cookieValue;
+    },
+
+    set: function(name, value, expires, path, domain, secure) {
+        var cookieText = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+        if (expires instanceof Date) {
+            cookieText += "; expires=" + expires.toGMTString();
+        }
+        if (path) {
+            cookieText += "; data-path=" + path;
+        }
+        if (domain) {
+            cookieText += "; domain=" + domain;
+        }
+        if (secure) {
+            cookieText += "; secure";
+        }
+        document.cookie = cookieText;
+    },
+
+    unset: function(name, path, domain, secure) {
+        this.set(name, "", new Date(0), path, domain, secure);
+    }
+
+};
+
+/**
+ * 子cookie操作
+ * @type {Object}
+ */
+var SubCookieUtil = {
+    set: function(name, subName, value, expires, path, domain, secure) {
+        var subcookies = this.getAll(name) || {};
+        subcookies[subName] = value;
+        this.setAll(name, subcookies, expires, path, domain, secure);
+    },
+    setAll: function(name, subcookies, expires, path, domain, secure) {
+        var cookieText = encodeURIComponent(name) + "=",
+            subcookieParts = new Array(),
+            subName;
+        for (subName in subcookies) {
+            if (subName.length > 0 && subcookies.hasOwnProperty(subName)) {
+                subcookieParts.push(encodeURIComponent(subName) + "=" +
+                    encodeURIComponent(subcookies[subName]));
+            }
+        }
+        if (cookieParts.length > 0) {
+            cookieText += subcookieParts.join("&");
+            if (expires instanceof Date) {
+                cookieText += "; expires=" + expires.toGMTString();
+            }
+            if (path) {
+                cookieText += "; path=" + path;
+            }
+            if (domain) {
+                cookieText += "; domain=" + domain;
+            }
+            if (secure) {
+                cookieText += "; secure";
+            }
+        } else {
+            cookieText += "; expires=" + (new Date(0)).toGMTString();
+        }
+        document.cookie = cookieText;
+    },
+    get: function(name, subName) {
+        var subCookies = this.getAll(name);
+        if (subCookies) {
+            return subCookies[subName];
+        } else {
+            return null;
+        }
+    },
+    getAll: function(name) {
+        var cookieName = encodeURIComponent(name) + "=",
+            cookieStart = document.cookie.indexOf(cookieName),
+            cookieValue = null,
+            cookieEnd,
+            subCookies,
+            i,
+            parts,
+            result = {};
+        if (cookieStart > -1) {
+            cookieEnd = document.cookie.indexOf(";", cookieStart);
+            if (cookieEnd == -1) {
+                cookieEnd = document.cookie.length;
+            }
+            cookieValue = document.cookie.substring(cookieStart + cookieName.length, cookieEnd);
+            if (cookieValue.length > 0) {
+                subCookies = cookieValue.split("&");
+                for (i = 0, len = subCookies.length; i < len; i++) {
+                    parts = subCookies[i].split("=");
+                    result[decodeURIComponent(parts[0])] =
+                        decodeURIComponent(parts[1]);
+                }
+                return result;
+            }
+        }
+        return null;
+    },
+    unset: function(name, subName, path, domain, secure) {
+        var subcookies = this.getAll(name);
+        if (subcookies) {
+            delete subcookies[subName];
+            this.setAll(name, subcookies, null, path, domain, secure);
+        }
+    },
+    unsetAll: function(name, path, domain, secure) {
+        this.setAll(name, null, new Date(0), path, domain, secure);
+    }
+};
